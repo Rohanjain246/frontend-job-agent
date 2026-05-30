09b34528a29c69554dec09121311ac05c9063cd4
Rohan <127478245+Rohanjain246@users.noreply.github.com>
2026-05-30 17:15:54 +0530

    Create email_report.py

---
+++ src/email_report.py
@@ -0,0 +1,35 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email_report(report_text):
+sender = os.getenv("EMAIL_USER")
+password = os.getenv("EMAIL_PASSWORD")
+receiver = os.getenv("REPORT_RECEIVER")
+
+
+if not sender or not password or not receiver:
+    print("Email secrets missing")
+    return
+
+msg = MIMEMultipart()
+
+msg["From"] = sender
+msg["To"] = receiver
+msg["Subject"] = "Daily Frontend Job Report"
+
+msg.attach(MIMEText(report_text, "plain"))
+
+try:
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender, password)
+    server.send_message(msg)
+    server.quit()
+
+    print("Email sent successfully")
+
+except Exception as e:
+    print("Email error:", e)
+
